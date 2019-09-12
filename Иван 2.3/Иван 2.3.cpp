1baad6cf0c31846b2f122090530465628d5a2e7e
sedelnikova <54905384+sedelnikova@users.noreply.github.com>
2019-09-12 21:52:01 +0300

    Добавьте файлы проекта.

---
+++ Иван 2.3/Иван 2.3.cpp
@@ -0,0 +1,27 @@
+﻿// Иван 2.3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float A, B, C, P, Q, S;
+	cout << "Введите координаты точек на числовой оси: ";
+
+	cin >> A >> B >> C;
+	if (A < C && C < B)
+	{
+		P = (C - A) * (B - C);
+		cout << "Произведение = " << P;
+	}
+	else
+	{
+		cout << "Пардон, данные некорректны";
+
+	}
+	return 0;
+}
